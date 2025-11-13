ff7ac034599ecd64bc65c753155656959ef0576d
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-13 18:51:15 +0100

    feat: build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema, { IssueData } from "./schema";
+import prisma from "@/lib/prisma";
+
+export async function POST(request: NextRequest) {
+  const body: IssueData = await request.json();
+
+  const result = schema.safeParse(body);
+  if (!result.success)
+    return NextResponse.json(result.error.issues[0].message, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
