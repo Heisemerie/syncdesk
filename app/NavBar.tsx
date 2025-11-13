f9863a96d133f780b7ff233d36a93b53c0cffdba
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-13 17:07:24 +0100

    feat: build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { ImBug } from "react-icons/im";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <ImBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-50 hover:text-zinc-400 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
