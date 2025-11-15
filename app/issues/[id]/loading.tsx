76461bafcfe6e0aa97a4133c32475078f12b4f35
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-15 06:34:20 +0100

    style: fix loading skeleton width

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -2,13 +2,13 @@ import { Flex, Card, Skeleton } from "@radix-ui/themes";
 
 const loading = () => {
   return (
-    <div className="max-w-2xl">
-      <Skeleton height="20px" />
+    <div className="max-w-4xl">
+      <Skeleton height="20px" width='100px'/>
       <Flex className="gap-3 my-2">
         <Skeleton width="4rem" height="20px" />
         <Skeleton width="8rem" height="20px" />
       </Flex>
-      <Card className="prose dark:prose-invert">
+      <Card className="prose dark:prose-invert max-w-full">
         <Skeleton height="20px" className="my-2" />
         <Skeleton height="20px" className="my-2" />
         <Skeleton height="20px" className="my-2" />
