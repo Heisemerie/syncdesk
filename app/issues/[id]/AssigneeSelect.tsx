86956de5fbde1dc2de839a6d764c1739c3919db3
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-17 13:41:19 +0100

    feat: populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,24 @@
+import prisma from "@/prisma/prisma";
 import { Select } from "@radix-ui/themes";
-import React from "react";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = async () => {
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="orange">Orange</Select.Item>
-          <Select.Item value="apple">Apple</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
