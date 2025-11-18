9197f908b406bf963af5a76111948257f724f570
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-18 18:32:39 +0100

    feat: build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import prisma from "@/prisma/prisma";
+import IssueSummary from "./IssueSummary";
 import LastestIssues from "./LastestIssues";
 
-export default function Home() {
-  return <LastestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  return (
+    <>
+      <LastestIssues />
+      <IssueSummary closed={closed} inProgress={inProgress} open={open} />
+    </>
+  );
 }
