9046790354ae0dd37d9fcab85d7d777bac4678d3
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-18 12:43:04 +0100

    feat: filter issues

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,16 +1,26 @@
+"use client";
 import { Select } from "@radix-ui/themes";
 import { Status } from "../generated/prisma";
+import { useRouter } from "next/navigation";
 
 const IssueStatusFilter = () => {
-  const statuses: { label: String; value: Status | "All" }[] = [
-    { label: "All", value: "All" },
+  const router = useRouter();
+  const statuses: { label: String; value: Status | "ALL" }[] = [
+    { label: "All", value: "ALL" },
     { label: "Open", value: "OPEN" },
     { label: "In Progress", value: "IN_PROGRESS" },
     { label: "Closed", value: "CLOSED" },
   ];
 
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        status = status === "ALL" ? "" : status;
+        const query = status ? `?status=${status}` : "";
+        router.push(`/issues${query}`);
+      }}
+      defaultValue="ALL"
+    >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status, index) => (
