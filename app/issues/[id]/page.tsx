3abd6eaedc629db486fcf72380c411cf39edf917
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-11-18 12:01:57 +0100

    refactor: remove unused imports on issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,12 @@
+import { authOptions } from "@/app/api/auth/authOptions";
 import prisma from "@/prisma/prisma";
 import { Box, Flex, Grid } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/authOptions";
-import AssigneeSelect from "./AssigneeSelect";
-import { is } from "zod/v4/locales";
 
 interface Props {
   params: Promise<{ id: string }>;
